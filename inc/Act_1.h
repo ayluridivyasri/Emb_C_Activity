76b43a49ee3ec6858c5749bfc371b84c02abc0cb
ayluridivyasri <ayluridivyasri@gmail.com>
2021-04-29 14:29:25 +0530

    commit

---
+++ inc/Act_1.h
@@ -0,0 +1,22 @@
+
+/**
+ * @file Act_1.h
+ * @author Divya Sri
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __Act_1_H_
+#define __Act_1_H_
+#define AVR_ATmega328
+
+/**
+ * Include files
+ */ 
+#include <avr/io.h>
+
+
+#endif //Act_1_H 
---
+++ inc/Act_2.h
@@ -0,0 +1,18 @@
+/**
+ * @file Act_2.h
+ * @author Divya Sri 
+ * @brief declaring the functions
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef Act_2_H_
+#define Act_2_H_
+
+uint16_t ReadADC(uint8_t ch);
+void InitADC();
+
+#endif // Act_2_H_
